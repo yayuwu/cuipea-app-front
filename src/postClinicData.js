92e6dff0376c02e453e248fd050f8fc9bd92ada2
Yael Correa <yscorrea10@gmail.com>
2024-08-26 17:56:43 -0300

    logica post para api lista

--- src/postClinicData.js
+++ src/postClinicData.js
@@ -23,6 +23,8 @@ let questions = [
     { id: 21, question: '¿Otros problemas de salud o condiciones que deban ser mencionadas?' }
 ];
 
+// Función para traer el template
+
 const formDataUrl = '../public/templates/questionContainer.hbs'
 
 const loadTemplate = async (url)=> {
@@ -30,14 +32,17 @@ const loadTemplate = async (url)=> {
     return res.text()
 }
 
+// Lógica para inyectar el template en el html
+
 loadTemplate(formDataUrl)
 .then(templateSource => { const questionTemplate = Handlebars.compile(templateSource)
-
     const allQuestions = questions.map(question => questionTemplate(question)).join('')
     const container = document.getElementById('form-questions')
     container.innerHTML = allQuestions
     hideDetails()
 })
+
+// Función para mostrar u ocultar los Detalles según cuál input esté checked
 
 const hideDetails = () => {
     const questionsContainer = document.querySelectorAll('.question-container')
@@ -65,27 +70,96 @@ const hideDetails = () => {
     })
 }
 
+// Lógica de validación del form
+
 const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    const url = 'http://localhost:8080/userQuestions/'
     let isValid = true
-    
+    let firstError = null
+
     const questionsContainer = document.querySelectorAll('.question-container')
     questionsContainer.forEach(question => {
         const errorMessage = question.querySelector(`small[id^="error-message-"]`)
         const radioYes = question.querySelector(`input[id^="true-"]`)
         const radioNo = question.querySelector(`input[id^="false-"]`)
+        const detailsInput = question.querySelector('textarea');
+        const questionParagraph = question.querySelector('p'); 
+
+        let questionText = questionParagraph.innerText || questionParagraph.textContent;
+        questionText = questionText.replace(/^\d+\.\s*/, ''); // Elimina el número inicial y el punto (e.g., "1. ")
+        questionText = questionText.replace(/\s*\*$/, ''); // Elimina el asterisco final si existe
+
+        // Lógica para ocultar y mostrar el erroMessage 
         
         if (!radioNo.checked && !radioYes.checked) {
             errorMessage.style.display = 'block'
             isValid = false
+
+            if(!firstError){
+                firstError = errorMessage
+            }
         } else {
             errorMessage.style.display = 'none'
         }
+
+        // Lógica para el POST
+
+        let response = null
+        let details= ''
+
+        if (radioYes.checked) {
+            response = true
+            details = detailsInput ? detailsInput.value : ''
+        } else if (radioNo.checked) {
+            response = false
+            details = ''
+        }
+
+        if(response !== null){
+            const data = {
+                question: questionText,
+                response: response,
+                details: details
+            }
+            postDataClinic(url, data)
+        }
     })
+
+    // Scroll hacia el error cuando no se completa algún campo
+
+    if(firstError){
+        firstError.scrollIntoView({behavior: 'smooth', block: 'center'})
+    }
+
+    // Submit
 
     if(isValid){
         form.submit()
         form.reset()
     }
 })
+
+// Función para el POST
+
+const postDataClinic = async (url, obj) => {
+    try {
+        const options = {
+            'method' : 'POST',
+            'headers' : {
+                'Content-type' : 'application/json'
+            },
+            'body': JSON.stringify(obj) 
+        }
+
+        const res = await fetch(url, options)
+        if (!res.ok) {
+            throw new Error('No se pudieron obtener los datos', res.status)
+        }
+        const data = await res.json()
+        console.log('Datos enviados con éxito: ', data)
+    } catch (error) {
+        console.error('Error en la solicitud: ', error)
+    }
+} 
