2bbcb27f5546f0704dcb994a9f6c4f4fdb3aeed9
Yael Correa <yscorrea10@gmail.com>
2024-11-13 11:56:06 -0300

    Configurando enrutamiento

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './',
+  base: './', // Esto permite que las rutas de scripts sean relativas
   build: {
     outDir: 'dist'
   },
