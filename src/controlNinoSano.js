ad2d9a9d025665b2679bdc418f2d06634df9d58c
Yael Correa <yscorrea10@gmail.com>
2024-11-13 12:47:04 -0300

    Configurando enrutamiento

--- src/controlNinoSano.js
+++ src/controlNinoSano.js
@@ -8,48 +8,34 @@ console.log('Template Botón')
 const urlButtonAgregar = './templates/buttonAgregar.hbs'
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadTemplate(urlButtonAgregar)
-    .then(template => {
-        const buttonTemplate = Handlebars.compile(template)
+    // Retrasa la ejecución del código 200ms (ajusta el tiempo según sea necesario)
+    setTimeout(() => {
+        loadTemplate(urlButtonAgregar)
+            .then(template => {
+                const buttonTemplate = Handlebars.compile(template)
+                const dataBtn = { text: 'control' }
+                const btnHtml = buttonTemplate(dataBtn)
 
-        // Botón que redirige a un enlace
-        const dataBtn = {
-            text: 'control',
-        }
+                const containerRedirect = document.querySelector('.btn-nino-sano')
+                if (containerRedirect) {
+                    containerRedirect.innerHTML = btnHtml
+                    const btnAgregarRedirect = containerRedirect.querySelector('.btn-agregar');
+                    btnAgregarRedirect.classList.add('btn-agregar-pink')
+                    btnAgregarRedirect.addEventListener('click', () => {
+                        window.location.href = '/agregar-control'
+                    });
+                }
 
-        const btnHtml = buttonTemplate(dataBtn)
-
-        const containerRedirect = document.querySelector('.btn-nino-sano')
-
-        console.log(containerRedirect)
-        if (containerRedirect) {
-            containerRedirect.innerHTML = btnHtml
-
-            const btnAgregarRedirect = containerRedirect.querySelector('.btn-agregar')
-            
-            btnAgregarRedirect.classList.add('btn-agregar-pink')
-            
-            btnAgregarRedirect.addEventListener('click', () => {
-                window.location.href = '/agregar-control'
+                const containerOnClick = document.querySelector('#btn-agregar-control')
+                if (containerOnClick) {
+                    containerOnClick.innerHTML = btnHtml
+                    const btnAgregarOnClick = containerOnClick.querySelector('.btn-agregar');
+                    btnAgregarOnClick.classList.add('btn-agregar-pink')
+                    btnAgregarOnClick.addEventListener('click', () => {
+                        alert('Formulario enviado')
+                    });
+                }
             })
-        }
-
-        // Botón que ejecuta una función onclick
-
-        const containerOnClick = document.querySelector('#btn-agregar-control')
-
-        console.log(containerOnClick)
-        if (containerOnClick) {
-            containerOnClick.innerHTML = btnHtml
-
-            const btnAgregarOnClick = containerOnClick.querySelector('.btn-agregar')
-
-            btnAgregarOnClick.classList.add('btn-agregar-pink')
-
-            btnAgregarOnClick.addEventListener('click', () => {
-                alert('Formulario enviado')
-            })
-        }
-    })
-    .catch(error => console.error('Error al cargar el template:', error));
+            .catch(error => console.error('Error al cargar el template:', error))
+    }, 200)
 })
