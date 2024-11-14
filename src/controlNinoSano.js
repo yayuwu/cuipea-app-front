9564dd29b28ef2b02e35263571ea8e491455cf73
Yael Correa <yscorrea10@gmail.com>
2024-11-14 12:22:17 -0300

    Validación de controles y vacunas

--- src/controlNinoSano.js
+++ src/controlNinoSano.js
@@ -1,15 +1,29 @@
-const loadTemplate = async (url) => {
-    const res = await fetch(url)
-    return res.text()
-}
+import loadTemplate from './loadTemplate'
 
-console.log('Template Botón')
+let controlesNinoSano = [ 
+    { edad: '2 meses', fecha: '2024-01-14', peso: 6.5, talla: 60, pc: 40, ta: '90/60', observaciones: 'Vacunación y seguimiento del desarrollo' }, 
+    { edad: '4 meses', fecha: '2024-03-14', peso: 7.5, talla: 65, pc: 42, ta: '95/65', observaciones: 'Revisión de hitos del desarrollo, alimentación complementaria' }, 
+    { edad: '6 meses', fecha: '2024-05-14', peso: 8.5, talla: 70, pc: 44, ta: '100/70', observaciones: 'Chequeo de nutrición y desarrollo, vacunación' }, 
+    { edad: '9 meses', fecha: '2024-08-14', peso: 9, talla: 75, pc: 46, ta: '105/75', observaciones: 'Evaluación del desarrollo motor, vacunas' }, 
+    { edad: '12 meses', fecha: '2024-11-14', peso: 10, talla: 78, pc: 47, ta: '110/75', observaciones: 'Chequeo general, vacunación de 1 año' } 
+]
 
-const urlButtonAgregar = './templates/buttonAgregar.hbs'
+const urlButtonAgregar = '/templates/buttonAgregar.hbs'
+const urlTable = '/templates/tableRowsControl.hbs'
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Retrasa la ejecución del código 200ms (ajusta el tiempo según sea necesario)
     setTimeout(() => {
+        // Carga de datos de la tabla
+        loadTemplate(urlTable)
+            .then(template => {
+                const tableTemplate = Handlebars.compile(template)
+                const tableHtml = controlesNinoSano.map(control => tableTemplate(control)).join('')
+                const containerTableBody = document.getElementById('rows-control')
+                if (containerTableBody) {
+                    containerTableBody.innerHTML = tableHtml
+                }
+            })
+        // Botones
         loadTemplate(urlButtonAgregar)
             .then(template => {
                 const buttonTemplate = Handlebars.compile(template)
--- src/vacunacion.js
+++ src/vacunacion.js
@@ -1,9 +1,34 @@
 import loadTemplate from './loadTemplate'
 
+let vacunas = [ 
+    { vacuna: 'Hepatitis B', dosis: '1ª dosis', edad: 'al nacer', fecha: '2024-11-14' }, 
+    { vacuna: 'BCG', dosis: 'única dosis', edad: 'al nacer', fecha: '2024-11-14' }, 
+    { vacuna: 'Polio', dosis: '1ª dosis', edad: '2 meses', fecha: '2025-01-14' }, 
+    { vacuna: 'Polio', dosis: '2ª dosis', edad: '4 meses', fecha: '2025-03-14' }, 
+    { vacuna: 'DTP', dosis: '1ª dosis', edad: '2 meses', fecha: '2025-01-14' }, 
+    { vacuna: 'DTP', dosis: '2ª dosis', edad: '4 meses', fecha: '2025-03-14' }, 
+    { vacuna: 'Hib', dosis: '1ª dosis', edad: '2 meses', fecha: '2025-01-14' }, 
+    { vacuna: 'Hib', dosis: '2ª dosis', edad: '4 meses', fecha: '2025-03-14' }, 
+    { vacuna: 'Neumococo', dosis: '1ª dosis', edad: '2 meses', fecha: '2025-01-14' }, 
+    { vacuna: 'Neumococo', dosis: '2ª dosis', edad: '4 meses', fecha: '2025-03-14' } 
+]
+
 const urlBtn = '/templates/buttonAgregar.hbs'
+const urlTable = '/templates/tableRowsVacunacion.hbs'
 
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
+        // Carga de datos en la tabla 
+        loadTemplate(urlTable)
+         .then(template => {
+            const tableTemplate = Handlebars.compile(template)
+            const tableHtml = vacunas.map(vacuna => tableTemplate(vacuna)).join('')
+            const containerVacunas = document.getElementById('rows-vacunacion')
+            if (containerVacunas) {
+                containerVacunas.innerHTML = tableHtml
+            }
+         })
+        // Botones
         loadTemplate(urlBtn)
          .then(template => {
             const btnTemplate = Handlebars.compile(template)
