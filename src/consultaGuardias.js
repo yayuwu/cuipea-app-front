db9ecf1db3964cc0cef1faef0a23dc70c8a73917
Carla Urueña <carla.uruena@hotmail.com>
2024-11-25 16:32:23 -0300

    actualizando guardias botones

--- src/consultaGuardias.js
+++ src/consultaGuardias.js
@@ -1,5 +1,6 @@
 import * as yup from 'yup'
 import loadTemplate from "./loadTemplate"
+import Swal from 'sweetalert2'
 
 let guardias = [
     { title: 'Guardia 1', date: '29/07/24'},
@@ -19,46 +20,60 @@ document.addEventListener('DOMContentLoaded', () => {
                 const boxTemplate = Handlebars.compile(template)
                 const boxHtml = guardias.map(guardias => boxTemplate(guardias)).join('')
 
+                const currentLocation = window.location.pathname
+
+                if (currentLocation === '/atencion-guardia') {
+                    Swal.fire({
+                        title: 'Cargando...',
+                        allowOutsideClick: false, // Evita que se cierre al hacer clic fuera
+                        didOpen: () => {
+                            Swal.showLoading() // Activa el spinner de carga
+                        },
+                    })
+                }
+
                 const containerBoxes = document.getElementById('consultaGuardiasContainer')
 
                 if(containerBoxes){
                     containerBoxes.innerHTML = boxHtml
                     const boxes = document.querySelectorAll('.box-container')
                     boxes.forEach(box => box.classList.add('green-box'))
+                    Swal.close()
                 }
             })
             .catch(err => console.log(err))
-        // Botones
-        loadTemplate(urlBtn)
-            .then(template => {
-                const btnTemplate = Handlebars.compile(template)
-                const data = {
-                    text: 'guardia'
-                }
-                const btnHtml = btnTemplate(data)
-                const containerBtn = document.getElementById('btn-consultas-guardias')
-                const containerBtnAgregarConsulta = document.getElementById('btn-agregar-guardia')
+        // // Botones
+        // loadTemplate(urlBtn)
+        //     .then(template => {
+        //         const btnTemplate = Handlebars.compile(template)
+        //         const data = {
+        //             text: 'consulta'
+        //         }
+        //         const btnHtml = btnTemplate(data)
+        //         const containerBtn = document.getElementById('btn-consultas-enfermedad')
+        //         const containerBtnAgregarConsulta = document.getElementById('btn-agregar-consulta')
 
-                if (containerBtn) {
-                    containerBtn.innerHTML = btnHtml
-                    const btnConsulta = containerBtn.querySelector('.btn-agregar')
-                    btnConsulta.classList.add('btn-agregar-green')
-                    btnConsulta.addEventListener('click', () => {
-                        window.location.href = '/agregar-guardia'
-                    })
-                }
+        //         if (containerBtn) {
+        //             containerBtn.innerHTML = btnHtml
+        //             const btnConsulta = containerBtn.querySelector('.btn-agregar')
+        //             btnConsulta.classList.add('btn-agregar-violet')
+        //             btnConsulta.addEventListener('click', () => {
+        //                 window.location.href = '/agregar-consulta'
+        //             })
+        //         }
 
-                if (containerBtnAgregarConsulta) {
-                    containerBtnAgregarConsulta.innerHTML = btnHtml
-                    const btnConsulta = containerBtnAgregarConsulta.querySelector('.btn-agregar')
-                    btnConsulta.classList.add('btn-agregar-green')
-                    btnConsulta.type = 'submit'
-                }
+        //         if (containerBtnAgregarConsulta) {
+        //             containerBtnAgregarConsulta.innerHTML = btnHtml
+        //             const btnConsulta = containerBtnAgregarConsulta.querySelector('.btn-agregar')
+        //             btnConsulta.classList.add('btn-agregar-violet')
+        //             btnConsulta.type = 'submit'
+        //         }
                 
-            })
+        //     })
+            
         // Envío de datos formulario
         const form = document.getElementById('guardia-form')
-        console.log('Formulario: ', form)
+        // console.log('Formulario: ', form)
         if (form) {
             form.addEventListener('submit', async (e) => {
                 e.preventDefault()
